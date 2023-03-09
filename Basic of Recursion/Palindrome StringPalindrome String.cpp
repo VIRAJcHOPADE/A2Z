80a55e581b021bd51ff4e0488d7f2f9b30c6f3a5
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-03-09 08:18:07 +0530

    2nd day

--- Basic of Recursion/Palindrome StringPalindrome String.cpp
+++ Basic of Recursion/Palindrome StringPalindrome String.cpp
@@ -49,3 +49,28 @@ int main()
     return 0;
 }
 // } Driver Code Ends
+
+
+// leetcode
+
+// is valid palindrome
+
+// bool isPalindrome(string s) {
+//         s = removeNoise(s);
+//         for(int i=0; i<s.size()/2; i++){
+//             if (s[i]!= s[s.size()-i-1]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+    
+//     string removeNoise(string& s){
+//         string d;
+//         for(int i=0; i<s.size(); i++){
+//             if(::isalpha(s[i]) || ::isdigit(s[i])){
+//                 d.push_back(::tolower(s[i]));
+//             }
+//         }
+//         return d;
+//     }
