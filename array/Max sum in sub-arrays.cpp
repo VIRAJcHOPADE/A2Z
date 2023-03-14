2ce4295f1154389114fea24cb28e85c45c4368fd
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-03-14 20:22:21 +0530

    7

---
+++ array/Max sum in sub-arrays.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+        long long pairWithMaxSum(long long arr[], long long N)
+    {
+        // Your code goes here
+         long long csum=0,msum=-1;
+         
+        for(int i=1;i<N;i++){
+            
+            csum=arr[i-1]+arr[i];
+            msum=max(csum,msum);
+            csum=0;
+            
+        }
+        return msum;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() 
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		ll a[n];
+	    for(ll i=0;i<n;i++)
+		    cin>>a[i];
+		Solution ob;
+	    cout<<ob.pairWithMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+
+// } Driver Code Ends
