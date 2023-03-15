138e369be7a24a76618b599cb9e89b5a1b8fc7c3
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-03-15 15:12:00 +0530

    8

---
+++ array/Next Permutation.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> nextPermutation(int N, vector<int> arr){
+        // code here
+        int i;int l;
+        for(i=N-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                break;
+            }
+        }
+            if(i<0){
+                reverse(arr.begin(),arr.end());
+            }
+            else{
+                for(l=N-1;l>i;l--){
+                    if(arr[l]>arr[i]){
+                        break;
+                    }
+                }
+            swap(arr[l],arr[i]);
+            reverse(arr.begin()+i+1,arr.end());
+            }
+        return arr;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        vector<int> ans = ob.nextPermutation(N, arr);
+        for(int u: ans)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
