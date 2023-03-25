e5162599af1e9c501e6fb9611fbf6155a2fdac78
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-03-26 05:29:44 +0530

    19

--- Binary Search/Rotation.cpp
+++ Binary Search/Rotation.cpp
@@ -10,20 +10,27 @@ class Solution{
 public:	
 	int findKRotation(int arr[], int n) {
 	    // code here
-	     if(n==1){
-           return 0;
-       }
-       
-       for(int i=1 ; i  < n-1 ; i ++){
-           if(arr[i]<arr[i-1] && arr[i]<arr[i+1]){
-               return i;
-           }
-       }
-       if(arr[n-1]<arr[n-2]){
-           return n-1;
-       }
-       return 0;
-	}
+	    if(arr[n-1]>arr[0]){
+	        return 0;
+	    }
+	    int start=0;
+	    int end=n-1;
+	    while(start<=end){
+	      int  mid=start+(end-start)/2;
+	        int next=(mid+1)%n;
+	        int pree=(mid+n-1)%n;
+	        if(arr[mid]<=arr[next]  && arr[mid]<=arr[pree]){
+	            return mid;
+	        }
+	        else if(arr[mid]>arr[end]){
+	            start=mid+1;
+	        }
+	        else if(arr[mid]<arr[end]){
+	            end=mid-1;
+	        }
+	    }
+	    
+	   }
 
 };
 
