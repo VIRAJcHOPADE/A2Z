0e8917e72f4f770630fab8a49c185cab255d2b15
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-04-14 07:52:09 +0530

    38

---
+++ Linked List/Doubly LinkedList/Doubly linked list Insertion at given position.cpp
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* a Node of the doubly linked list */
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x)
+  {
+      data = x;
+      next = prev = NULL;
+  }
+};
+
+void addNode(Node *head, int pos, int data);
+
+Node *insert(Node *head, int x)
+{
+    if (head == NULL)
+    {
+        return new Node(x);
+    }
+    Node *n = new Node(x);
+    
+    head->next = n;
+    n->prev = head;
+    head = n;
+    return head;
+}
+
+void printList(Node *head)
+{
+  // The purpose of below two loops is
+  // to test the created DLL
+  Node *temp=head;
+  if (temp != NULL) {
+ 
+  while (temp->next!=NULL)
+    temp=temp->next;
+  while (temp->prev!=NULL)
+   temp = temp->prev;
+  }
+  while (temp != NULL)
+  {
+      printf("%d ",temp->data);
+      temp=temp->next;
+  }
+  
+  cout << endl;
+}
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--)
+  {
+  Node *head = NULL; 
+  Node *root = NULL;
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+     int x;
+     scanf("%d",&x);
+     head = insert(head, x);
+     if(root==NULL) root = head;
+  }     
+  head = root;
+  int pos,data;
+  cin>>pos>>data;
+  addNode(head, pos, data);
+  printList(head);
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+//Function to insert a new node at given position in doubly linked list.
+void addNode(Node *head, int pos, int data)
+{
+   // Your code here
+    Node* temp= head;
+   Node* newNode= new Node(data);
+   if(head==NULL){
+       return;
+   }
+   else{
+   int count=0;
+   while(count!=pos){
+       temp=temp->next;
+       count++;
+   }
+   }
+   if(temp->next==NULL){
+       temp->next=newNode;
+       newNode->prev=temp;
+       newNode->next=NULL;
+       return;
+   }
+   else{
+       newNode-> next=temp->next;
+       newNode->prev=temp;
+       temp->next=newNode;
+       temp->next->prev=newNode;
+   }
+}
