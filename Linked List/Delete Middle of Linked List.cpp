84d2be93856292147b0c00ba8673a99f5794c2c0
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-04-21 06:48:38 +0530

    45

---
+++ Linked List/Delete Middle of Linked List.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to get the middle of the linked list*/
+struct Node *deleteMid(struct Node *head);
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		head = deleteMid(head);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+
+
+// } Driver Code Ends
+
+
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+Node* deleteMid(Node* head)
+{
+    // Your Code Here
+    int n=0;
+    Node *temp = head;
+    
+    while(temp){
+        n++;
+        temp = temp -> next;
+    }
+    temp =head;
+    int mid = n/2 + 1;
+    if(n%2 ==0){
+        mid= (n + 2) / 2;
+    }
+    while(mid){
+       if(mid==2){
+           temp -> next = temp -> next -> next;
+           temp = temp -> next;
+           return head;
+       } 
+       mid--;
+       temp = temp-> next;
+    }
+}
