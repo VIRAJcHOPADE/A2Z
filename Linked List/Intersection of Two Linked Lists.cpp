c331c73abba1befd39390da08fa140a3d3866146
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-04-23 05:45:44 +0530

    47

---
+++ Linked List/Intersection of Two Linked Lists.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+
+// } Driver Code Ends
+/* structure of list node:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution{
+  public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        // code here
+       unordered_map<int,int> ump;
+      Node *temp1=head2;
+      int l=0;
+      while(temp1)
+      {
+          ump[temp1->data]=l;
+          l++;
+          temp1=temp1->next;
+      }
+      Node *ans=new Node(0);
+      Node *temp=ans;
+      while(head1)
+      {
+          if(ump.find(head1->data)!=ump.end())
+          {
+              Node *var = new Node(head1->data);
+              ans->next=var;
+              ans=var;
+          }
+          head1=head1->next;
+      }
+      return temp->next;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    
+	    cin>> n;
+	    Node* head1 = inputList(n);
+	    
+	    cin>>m;
+	    Node* head2 = inputList(m);
+	    Solution obj;
+	    Node* result = obj.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
