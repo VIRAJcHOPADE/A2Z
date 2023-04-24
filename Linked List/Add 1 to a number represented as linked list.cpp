ba35d65769b0118c22db9f4913b2d764d0255c02
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-04-24 21:59:06 +0530

    48

--- Linked List/Add 1 to a number represented as linked list.cpp
+++ Linked List/Add 1 to a number represented as linked list.cpp
@@ -48,8 +48,7 @@ class Solution
     public:
     Node* addOne(Node *head) 
     {
-        // Your Code here
-        // return head of list after adding one
+        
         if(!head) return head;
        if(head->next==NULL){
            head->data+=1;
