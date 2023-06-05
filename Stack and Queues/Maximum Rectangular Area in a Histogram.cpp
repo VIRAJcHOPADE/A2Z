b5bfc269d8c66d6f79c6b83042d03f651464438d
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-06-05 20:20:25 +0530

    S & Q

---
+++ Stack and Queues/Maximum Rectangular Area in a Histogram.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find largest rectangular area possible in a given histogram.
+    long long getMaxArea(long long arr[], int n)
+    {
+        // Your code here
+        stack<int>st;
+        long long res = 0;
+        int tp;
+        long long curr;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && arr[st.top()]>=arr[i])
+            {
+                tp = st.top();
+                st.pop();
+                curr = arr[tp]*((st.empty())?(i):(i-st.top()-1));
+                res = max(res,curr);
+            }
+            st.push(i);
+        }
+        while(!st.empty())
+        {
+            tp = st.top();
+            st.pop();
+            curr = arr[tp]*((st.empty())?(n):(n-st.top()-1));
+            res = max(curr,res);
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        long long arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.getMaxArea(arr, n)<<endl;
+    
+    }
+	return 0;
+}
+
+// } Driver Code Ends
