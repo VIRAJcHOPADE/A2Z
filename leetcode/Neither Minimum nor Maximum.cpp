c5ff2d4001b167deb30f80b66305b92fe4b8ab0e
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-06-11 23:01:37 +0530

    349

---
+++ leetcode/Neither Minimum nor Maximum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        
+        int n = nums.size();
+    
+    if (n <= 2) {
+        return -1;
+    }
+    
+    sort(nums.begin(), nums.end());
+    
+    return nums[1];
+    }
+};
